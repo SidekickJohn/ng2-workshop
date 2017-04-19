74561868839df48e820c86baca314a60e066b398
schulung <schulung>
2017-04-19 13:23:53 +0200

    Added Passenger Search and Add Passenger functionality

---
+++ passagier_demo/src/app/passengersearch/passenger-search.component.ts
@@ -0,0 +1,94 @@
+import { Http, Headers, URLSearchParams } from '@angular/http';
+import { Passenger } from './../../entities/passengers';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'passenger-search',
+  templateUrl: './passenger-search.component.html'  
+})
+export class PassengerSearchComponent implements OnInit {
+  
+  title = 'Passenger Search!';
+  passengers: Array<Passenger> = [];
+  name: string;
+  firstName: string;
+  selectedPassenger: Passenger;
+
+constructor(private http: Http){}
+
+  ngOnInit() { 
+        console.debug('Komponente ist jetzt da!');
+    }
+ 
+  
+  search(): void {
+    
+    let url ="http://www.angular.at/api/passenger";
+    let headers= new Headers();
+    headers.set('Accept', 'application/json');
+
+    let search = new URLSearchParams();
+    search.set('name', this.name);
+    search.set('firstName', this.firstName);
+
+    this
+    .http
+    .get(url, {search, headers})
+    .map(resp => resp.json())
+    .subscribe(
+      passengers => {
+        this.passengers = passengers;
+      },
+      err => {
+        console.error('Fehler oider!! Bissd deppert?', err);
+      }
+    )
+  }
+
+  select(p: Passenger): void {
+    this.selectedPassenger = p;
+  }
+
+ addNewPassenger(passenger: Passenger): void {
+   if(passenger.id != 0){
+    passenger.id = 0;
+   }
+   this.savePassenger(passenger);
+ }
+
+
+
+  savePassenger(passenger: Passenger): void {
+    let url ="http://www.angular.at/api/passenger";
+    let headers= new Headers();
+    headers.set('Accept', 'application/json');
+
+    this
+    .http
+    .post(url, passenger, {headers})
+    .map(resp => resp.json())
+    .subscribe(
+      success =>{
+        console.log("Updated successfully: ", passenger, success);
+        this.unsetSelectedPassenger();
+      },      
+      err => {
+        console.error('Fehler oider!! Bissd deppert?', err);
+      }
+    )
+  }
+
+  unsetSelectedPassenger():void{
+    this.selectedPassenger = null;
+  }
+
+  createNewPass():void{
+    this.selectedPassenger = {
+      id: 0,
+      name: this.name,
+      firstName: this.firstName,
+      bonusMiles: 0,
+      passengerStatus: ""
+    }
+  }
+}
